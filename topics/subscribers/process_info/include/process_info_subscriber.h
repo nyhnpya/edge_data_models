3a5003a0c83ecd61aefd234cbda2ea269e806db7
Mark Carrier <mark.carrier@ensignenergy.com>
2017-06-22 14:01:44 -0500

    Initial commit of subscriber/process_info library

---
+++ topics/subscribers/process_info/include/process_info_subscriber.h
@@ -0,0 +1,49 @@
+#ifndef __PROCESS_INFO_SUBSCRIBER_H__
+#define __PROCESS_INFO_SUBSCRIBER_H__
+
+#include "subscriber.h"
+#include "process_info.h"
+#include "process_infoSupport.h"
+
+class CProcessInfoSubscriber : public TSubscriber< ProcessMonitor::ProcessInfo>
+{
+public:
+    typedef std::function<void(const ProcessMonitor::ProcessInfo &data)> OnDataAvailableEvent;
+
+    CProcessInfoSubscriber();
+    virtual ~CProcessInfoSubscriber();
+
+    bool Create(int32_t domain);
+    void OnDataAvailable(OnDataAvailableEvent event);
+    void OnDataDisposed(OnDataDisposedEvent event);
+    void OnLivelinessChanged(OnLivelinessChangedEvent event);
+    void OnSubscriptionMatched(OnSubscriptionMatchedEvent event);
+    bool ValidData();
+
+    // Topic getters
+    bool GetProcessName(const char *processName);
+    bool GetPid(int32_t &pid);
+    bool GetTotalVirtualMemaory(int64_t &totalVirtualMemory);
+    bool GetUsedVirtualMemory(int64_t &usedVirtualMemory);
+    bool GetTotalPhysicalMemaory(int64_t &totalPhysicalMemory);
+    bool GetUsedPhysicalMemory(int64_t &usedPhysicalMemory);
+
+protected:
+    ///Derived Methods
+    void DataAvailable(const ProcessMonitor::ProcessInfo &data,
+                       const DDS::SampleInfo &sampleInfo);
+    void DataDisposed(const DDS::SampleInfo &sampleInfo);
+    void LivelinessChanged(const DDS::LivelinessChangedStatus &status);
+    void SubscriptionMatched(const DDS::SubscriptionMatchedStatus &status);
+
+private:
+    ProcessMonitor::ProcessInfo  m_data;
+    DDS::SampleInfo              m_sampleInfo;
+    DDS::LivelinessChangedStatus m_livelinessStatus;
+    OnDataAvailableEvent         m_pOnDataAvailable;
+    OnDataDisposedEvent          m_pOnDataDisposed;
+    OnLivelinessChangedEvent     m_pOnLivelinessChanged;
+    OnSubscriptionMatchedEvent   m_pOnSubscriptionMatched;
+};
+
+#endif // __PROCESS_INFO_SUBSCRIBER_H__
---
+++ topics/subscribers/process_info/process_info_subscriber.cpp
@@ -0,0 +1,130 @@
+#include <string.h>
+#include "process_info_subscriber.h"
+
+CProcessInfoSubscriber::CProcessInfoSubscriber()
+{
+}
+
+CProcessInfoSubscriber::~CProcessInfoSubscriber()
+{
+}
+
+bool CProcessInfoSubscriber::ValidData()
+{
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetProcessName(const char *processName)
+{
+    processName = m_data.processName;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetPid(int32_t &pid)
+{
+    pid = m_data.pid;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetTotalVirtualMemaory(int64_t &totalVirtualMemory)
+{
+    totalVirtualMemory = m_data.totalVirtualMemory;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetUsedVirtualMemory(int64_t &usedVirtualMemory)
+{
+    usedVirtualMemory = m_data.usedVirtualMemory;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetTotalPhysicalMemaory(int64_t &totalPhysicalMemory)
+{
+    totalPhysicalMemory = m_data.totalPhysicalMemory;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::GetUsedPhysicalMemory(int64_t &usedPhysicalMemory)
+{
+    usedPhysicalMemory = m_data.usedPhysicalMemory;
+
+    return (m_sampleInfo.valid_data == DDS_BOOLEAN_TRUE);
+}
+
+bool CProcessInfoSubscriber::Create(int32_t domain)
+{
+    return TSubscriber::Create(domain,
+                               ProcessMonitor::PROCESS_INFO,
+                               "EdgeBaseLibrary",
+                               "EdgeBaseProfile");
+}
+
+void CProcessInfoSubscriber::OnDataAvailable(OnDataAvailableEvent event)
+{
+    m_pOnDataAvailable = event;
+}
+
+void CProcessInfoSubscriber::OnDataDisposed(OnDataDisposedEvent event)
+{
+    m_pOnDataDisposed = event;
+}
+
+void CProcessInfoSubscriber::OnLivelinessChanged(OnLivelinessChangedEvent event)
+{
+    m_pOnLivelinessChanged = event;
+}
+
+void CProcessInfoSubscriber::OnSubscriptionMatched(OnSubscriptionMatchedEvent event)
+{
+    m_pOnSubscriptionMatched = event;
+}
+
+void CProcessInfoSubscriber::DataAvailable(const ProcessMonitor::ProcessInfo &data,
+                                           const DDS::SampleInfo &sampleInfo)
+{
+    m_sampleInfo = sampleInfo;
+
+    if (sampleInfo.valid_data == DDS_BOOLEAN_TRUE)
+    {
+        m_data = data;
+
+        if (m_pOnDataAvailable != nullptr)
+        {
+            m_pOnDataAvailable(data);
+        }
+    }
+}
+
+void CProcessInfoSubscriber::DataDisposed(const DDS::SampleInfo &sampleInfo)
+{
+    LOG_INFO("Sample disposed");
+    m_sampleInfo = sampleInfo;
+
+    if (m_pOnDataDisposed != nullptr)
+    {
+        m_pOnDataDisposed(sampleInfo);
+    }
+}
+
+void CProcessInfoSubscriber::LivelinessChanged(const DDS::LivelinessChangedStatus &status)
+{
+    LOG_INFO("Liveliness lost");
+    if (m_pOnLivelinessChanged != nullptr)
+    {
+        m_pOnLivelinessChanged(status);
+    }
+}
+
+void CProcessInfoSubscriber::SubscriptionMatched(const DDS::SubscriptionMatchedStatus &status)
+{
+    LOG_INFO("Subscription matched");
+    if (m_pOnSubscriptionMatched != nullptr)
+    {
+        m_pOnSubscriptionMatched(status);
+    }
+}
