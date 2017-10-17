53e141b287682f0e5f2241ebc884d2f087bbd68f
Mark Carrier <mark@carrierlabs.com>
2017-10-17 13:52:50 -0500

    Added Driling Test to test suite

---
+++ tests/drilling_test/publisher/drilling_publisher.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <stdint.h>
+#ifdef _LINUX
+#include <unistd.h>
+#endif
+#include <signal.h>
+#include <string.h>
+#include <thread>
+#include "cmdparser.h"
+#include "drilling_request_publisher.h"
+
+bool gTerminate = false;
+
+CDrillingRequestPublisher *gpStatePublisher = nullptr;
+std::thread threadId;
+
+#ifdef _LINUX
+void SignalHandler(int32_t signal)
+{
+    /// Signal handler for SIGINT, SIGABRT, SIGFPE, SIGILL, SIGSEGV, SIGHUP 
+    switch (signal)
+    {
+        case SIGINT:
+            std::cout << "Signal SIGINT received." << std::endl;
+            break;
+        case SIGTERM:
+            std::cout << "Signal SIGTERM received." << std::endl;
+            break;
+        case SIGABRT:
+            std::cout << "Signal SIGABRT received." << std::endl;
+            break;
+        case SIGFPE:
+            std::cout << "Signal SIGFPE received." << std::endl;
+            break;
+        case SIGILL:
+            std::cout << "Signal SIGGILL received." << std::endl;
+            break;
+        case SIGSEGV:
+            std::cout << "Signal SIGSEGV received." << std::endl;
+            break;
+        case SIGHUP:
+            std::cout << "Signal SIGHUB received." << std::endl;
+            break;
+        default:
+            std::cout << "Undhandled Signal received: " << signal  << std::endl;
+            break;
+    }
+
+    gTerminate = true;
+
+    return;
+}
+
+void register_signal_handler()
+{
+    struct sigaction sigact;  /// Signal mask
+
+    sigact.sa_handler = SignalHandler;
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+
+    sigaction(SIGINT,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGABRT, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGFPE,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGILL,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGHUP,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGTERM, &sigact, (struct sigaction *)NULL);
+}
+#endif
+
+//    void SetRopMode(const bool ropMode);
+//    void SetWobMode(const bool wobMode);
+//    void SetDifferentialPressureMode(const bool differentialPressureMode);
+//    void SetTorqueMode(const bool torqueMode);
+void set_rop()
+{
+    double ropLimit;
+
+    std::cout << "ROP Limit: ";
+    std::cin >> ropLimit;
+    gpStatePublisher->SetRopLimit((meters_per_second_t)ropLimit);
+}
+
+void set_wob()
+{
+    double wobLimit;
+
+    std::cout << "WOB Limit: ";
+    std::cin >> wobLimit;
+    gpStatePublisher->SetWobLimit((newton_t)wobLimit);
+}
+
+void set_diffp()
+{
+    double diffpLimit;
+
+    std::cout << "DiffP Limit: ";
+    std::cin >> diffpLimit;
+    gpStatePublisher->SetDifferentialPressureLimit((pascal_t)diffpLimit);
+}
+
+void set_torque()
+{
+    double torqueLimit;
+
+    std::cout << "Torque Limit: ";
+    std::cin >> torqueLimit;
+    gpStatePublisher->SetTorqueLimit((newton_meter_t)torqueLimit);
+}
+
+void top_level_menu()
+{
+    char choice;
+
+    do
+    {
+        std::cout << std::endl;
+        std::cout << "Objective State Publisher" << std::endl;
+        std::cout << "------------------------" << std::endl;
+        std::cout << "1. Publish drilling parameters" << std::endl;
+        std::cout << "q. exit" << std::endl;
+        std::cout << "option: ";
+        std::cin >> choice;
+        std::cout << std::endl;
+
+        switch (choice)
+        {
+            case 'q':
+                gTerminate = true;
+
+                if (threadId.joinable())
+                {
+                    threadId.join();
+                }
+                break;
+            case '1':
+                set_rop();
+                set_wob();
+                set_diffp();
+                set_torque();
+                gpStatePublisher->PublishSample();
+                break;
+        }
+    } while (gTerminate == false);
+
+}
+
+int32_t main(int32_t argc, char **argv)
+{
+    cli::Parser              parser(argc, argv);
+    int32_t                  domain;
+
+	register_signal_handler();
+
+    parser.set_optional<std::string>("c", "configFile", "pipe_handler.conf", "External configuration file.");
+    parser.set_optional<int32_t>("d", "domain", 100, "DDS Domain.");
+    parser.run_and_exit_if_error();
+
+    domain = parser.get<int32_t>("d");
+
+    CDomainParticipant::Instance()->SetQosFile("USER_QOS_PROFILES.xml", "EdgeBaseLibrary", "EdgeBaseProfile");
+    CDomainParticipant::Instance()->Create(domain);
+
+    gpStatePublisher = new CDrillingRequestPublisher();
+
+    if (gpStatePublisher->Create(domain) == true)
+    {
+        gpStatePublisher->CreateInstance();
+        top_level_menu();
+    }
+
+    std::cout << "Waiting for thread: "  << std::endl;
+
+    if (threadId.joinable())
+    {
+        threadId.join();
+    }
+
+    gpStatePublisher->Destroy();
+}
---
+++ tests/drilling_test/subscriber/objective_subscriber.cpp
@@ -0,0 +1,180 @@
+#include <ncurses.h>
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include "cmdparser.h"
+#include "objective_subscriber.h"
+
+bool gTerminate = false;
+bool gCreated = true;
+
+void SignalHandler(int32_t signal)
+{
+    /// Signal handler for SIGINT, SIGABRT, SIGFPE, SIGILL, SIGSEGV, SIGHUP 
+    switch (signal)
+    {
+        case SIGINT:
+            std::cout << "Signal SIGINT received." << std::endl;
+            break;
+        case SIGTERM:
+            std::cout << "Signal SIGTERM received." << std::endl;
+            break;
+        case SIGABRT:
+            std::cout << "Signal SIGABRT received." << std::endl;
+            break;
+        case SIGFPE:
+            std::cout << "Signal SIGFPE received." << std::endl;
+            break;
+        case SIGILL:
+            std::cout << "Signal SIGGILL received." << std::endl;
+            break;
+        case SIGSEGV:
+            std::cout << "Signal SIGSEGV received." << std::endl;
+            break;
+        case SIGHUP:
+            std::cout << "Signal SIGHUB received." << std::endl;
+            break;
+        default:
+            std::cout << "Undhandled Signal received: " << signal  << std::endl;
+            break;
+    }
+
+    gTerminate = true;
+
+    return;
+}
+
+void register_signal_handler()
+{
+    struct sigaction sigact;  /// Signal mask
+
+    sigact.sa_handler = SignalHandler;
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+
+    sigaction(SIGINT,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGABRT, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGFPE,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGILL,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGHUP,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGTERM, &sigact, (struct sigaction *)NULL);
+}
+
+void get_objective(const Plan::ObjectiveState &data)
+{
+    double value; 
+
+    mvprintw(2, 1, "Objective: [%d]", data.objective);
+    mvprintw(4, 1, "Timestamp: [%d]:[%d]", data.timestamp.sec, data.timestamp.nanosec);
+    mvprintw(8, 1, "Status [%s]", gCreated ? "Created  ": "Destroyed");
+
+    refresh();
+}
+
+void update_state()
+{
+    mvprintw(8, 1, "Status [%s]", gCreated ? "Created  ": "Destroyed");
+
+    refresh();
+}
+
+void invalid_data()
+{
+    double value; 
+
+    mvprintw(2, 1, "Objective: [-]");
+    mvprintw(8, 1, "Status [%s]", gCreated ? "Created  ": "Destroyed");
+
+    refresh();
+}
+
+void liveliness_changed(const DDS::LivelinessChangedStatus &status)
+{
+    if (status.alive_count == 0)
+    {
+        invalid_data();
+    }
+}
+
+void data_disposed(const DDS::SampleInfo &sampleInfo)
+{
+    if (sampleInfo.valid_data == DDS_BOOLEAN_FALSE)
+    {
+        invalid_data();
+    }
+}
+
+int32_t main(int32_t argc, char **argv)
+{
+    cli::Parser parser(argc, argv);
+    int32_t     domain;
+
+    //    register_signal_handler();
+    parser.set_optional<std::string>("c", "configFile", "pipe_handler.conf", "External configuration file.");
+    parser.set_optional<int32_t>("d", "domain", 100, "DDS Domain.");
+    parser.run_and_exit_if_error();
+
+    domain = parser.get<int32_t>("d");
+
+    initscr();
+
+    CDomainParticipant::Instance()->SetQosFile("USER_QOS_PROFILES.xml", "EdgeBaseLibrary", "EdgeBaseProfile");
+    CDomainParticipant::Instance()->Create(domain);
+
+    CObjectiveStateSubscriber *pSubscriber = new CObjectiveStateSubscriber();
+
+    if (pSubscriber->Create(domain) == true)
+    {
+        pSubscriber->OnDataAvailable([&](const Plan::ObjectiveState &data)
+                                     {
+                                         get_objective(data);
+                                     });
+
+        pSubscriber->OnLivelinessChanged([&](const DDS::LivelinessChangedStatus &status)
+                                         {
+                                             liveliness_changed(status);
+                                         });
+
+        pSubscriber->OnDataDisposed([&](const DDS::SampleInfo &sampleInfo)
+                                    {
+                                        data_disposed(sampleInfo);
+                                    });
+
+        invalid_data();
+        while (gTerminate == false)
+        {
+            char choice;
+
+            std::cin >> choice;
+
+            switch (choice)
+            {
+                case 'q':
+                    gTerminate = true;
+                    break;
+                case 'c':
+                    //                    if (gCreated == false)
+                    {
+                        pSubscriber->Create(domain);
+                        gCreated = true;
+                        update_state();
+                    }
+                    break;
+                case 'd':
+                    //                    if (gCreated == true)
+                    {
+                        pSubscriber->Destroy();
+                        gCreated = false;
+                        update_state();
+                    }
+                    break;
+            }            
+        }
+    }
+
+    endwin();
+
+    pSubscriber->Destroy();
+}
