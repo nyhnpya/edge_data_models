f5eb5e19ee9e5a800f42f5f87e9dc3ec1203449a
Mark Carrier <mark@carrierlabs.com>
2017-12-27 09:14:53 -0600

    Added copyright to all header files

--- topics/objective_state/objective/include/objective_state_publisher.h
+++ topics/objective_state/objective/include/objective_state_publisher.h
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2017 Ensign Energy Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Ensign Energy Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Ensign Energy Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Ensign Energy Incorporated.
+ */
 #ifndef __OBJECTIVE_STATE_PUBLISHER_H__
 #define __OBJECTIVE_STATE_PUBLISHER_H__
 
