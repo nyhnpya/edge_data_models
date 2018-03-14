79db2d214decf172a4a0d48299de7b58ea7b3e5e
Mark Carrier <mark@carrierlabs.com>
2018-03-14 15:31:22 -0500

    Added state publisher/subscriber template code

---
+++ topics/objective_state/develop/template_state_publisher.cpp
@@ -0,0 +1,41 @@
+#include "template_state_publisher.h"
+
+CTemplateStatePublisher::CTemplateStatePublisher()
+{
+}
+
+CTemplateStatePublisher::~CTemplateStatePublisher()
+{
+    DDS_String_free(m_pDataInstance->id);
+}
+
+bool CTemplateStatePublisher::Initialize()
+{
+    CDdsUuid uuid;
+
+    uuid.GenerateUuid();
+    m_pDataInstance->id = DDS_String_dup(uuid.c_str());
+
+    return true;
+}
+
+bool CTemplateStatePublisher::PublishSample()
+{
+    bool bRetVal = false;
+    DDS_Time_t currentTime;
+
+    GetParticipant()->get_current_time(currentTime);
+    m_pDataInstance->timestamp.sec = currentTime.sec;
+    m_pDataInstance->timestamp.nanosec = currentTime.nanosec;
+    bRetVal = Publish();
+
+    return bRetVal;
+}
+
+bool CTemplateStatePublisher::Create(int32_t domain)
+{
+    return TPublisher::Create(domain,
+                              nec::process::TEMPLATE_STATE,
+                              "EdgeBaseLibrary",
+                              "EdgeBaseProfile");
+}
---
+++ topics/objective_state/develop/template_state_subscriber.h
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2018 Ensign Energy Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Ensign Energy Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Ensign Energy Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Ensign Energy Incorporated.
+ */
+#ifndef __TEMPLATE_STATE_SUBSCRIBER_H__
+#define __TEMPLATE_STATE_SUBSCRIBER_H__
+
+#include <mutex>
+#include "subscriber.h"
+#include "template.h"
+#include "templateSupport.h"
+#ifdef _WIN32
+#undef pascal
+#endif
+#include "units.h"
+
+using namespace units;
+
+class CTemplateStateSubscriber : public TSubscriber< nec::process::TemplateState>
+{
+public:
+    CTemplateStateSubscriber();
+    virtual ~CTemplateStateSubscriber();
+
+    bool Create(int32_t domain);
+    void OnDataAvailable(OnDataAvailableEvent event);
+    void OnDataDisposed(OnDataDisposedEvent event);
+    void OnLivelinessChanged(OnLivelinessChangedEvent event);
+    void OnSubscriptionMatched(OnSubscriptionMatchedEvent event);
+    bool ValidData();
+
+    // Topic getters
+
+protected:
+    ///Derived Methods
+    void DataAvailable(const nec::process::TemplateState &data,
+                       const DDS::SampleInfo &sampleInfo);
+    void DataDisposed(const DDS::SampleInfo &sampleInfo);
+    void LivelinessChanged(const DDS::LivelinessChangedStatus &status);
+    void SubscriptionMatched(const DDS::SubscriptionMatchedStatus &status);
+
+private:
+    nec::process::TemplateState  m_data;
+    DDS::SampleInfo              m_sampleInfo;
+    DDS::LivelinessChangedStatus m_livelinessStatus;
+    OnDataAvailableEvent         m_pOnDataAvailable;
+    OnDataDisposedEvent          m_pOnDataDisposed;
+    OnLivelinessChangedEvent     m_pOnLivelinessChanged;
+    OnSubscriptionMatchedEvent   m_pOnSubscriptionMatched;
+};
+
+#endif // __TEMPLATE_STATE_SUBSCRIBER_H__
