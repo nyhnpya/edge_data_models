162d631eadcd7084e88e94745e943e818117fb06
Mark Carrier <mark@carrierlabs.com>
2019-04-08 08:13:01 -0400

    New test publisher for ROPTIMIZSER

---
+++ tests/roptimizer_config_test/publisher/rop_config_publisher.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <stdint.h>
+#ifdef _LINUX
+#include <unistd.h>
+#endif
+#include <signal.h>
+#include <string.h>
+#include <thread>
+#include "cmdparser.h"
+#include "roptimizer_configuration_request_publisher.h"
+
+bool gTerminate = false;
+
+CRoptimizerConfigurationRequestPublisher::CConfigurationStateRequestPublisher *gpStatePublisher = nullptr;
+std::thread threadId;
+
+#ifdef _LINUX
+void SignalHandler(int32_t signal)
+{
+    /// Signal handler for SIGINT, SIGABRT, SIGFPE, SIGILL, SIGSEGV, SIGHUP 
+    switch (signal)
+    {
+        case SIGINT:
+            std::cout << "Signal SIGINT received." << std::endl;
+            break;
+        case SIGTERM:
+            std::cout << "Signal SIGTERM received." << std::endl;
+            break;
+        case SIGABRT:
+            std::cout << "Signal SIGABRT received." << std::endl;
+            break;
+        case SIGFPE:
+            std::cout << "Signal SIGFPE received." << std::endl;
+            break;
+        case SIGILL:
+            std::cout << "Signal SIGGILL received." << std::endl;
+            break;
+        case SIGSEGV:
+            std::cout << "Signal SIGSEGV received." << std::endl;
+            break;
+        case SIGHUP:
+            std::cout << "Signal SIGHUB received." << std::endl;
+            break;
+        default:
+            std::cout << "Undhandled Signal received: " << signal  << std::endl;
+            break;
+    }
+
+    gTerminate = true;
+
+    return;
+}
+
+void register_signal_handler()
+{
+    struct sigaction sigact;  /// Signal mask
+
+    sigact.sa_handler = SignalHandler;
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+
+    sigaction(SIGINT,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGABRT, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGFPE,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGILL,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL);
+    sigaction(SIGHUP,  &sigact, (struct sigaction *)NULL);
+    sigaction(SIGTERM, &sigact, (struct sigaction *)NULL);
+}
+#endif
+
+void set_steady_state_window()
+{
+    int32_t input;
+
+    std::cout << "Steady State Window: ";
+    std::cin >> input;
+    gpStatePublisher->SetSteadyStateWindow(input);
+}
+
+void set_steady_state_minimum()
+{
+    int32_t input;
+
+    std::cout << "Steady State Min: ";
+    std::cin >> input;
+    gpStatePublisher->SetSteadyStateMin(input);
+}
+
+void set_rpm_step_size()
+{
+    int32_t input;
+
+    std::cout << "Rpm Step Size: ";
+    std::cin >> input;
+    gpStatePublisher->SetRpmStepSize(input);
+}
+
+void set_rop_step_size()
+{
+    int32_t input;
+
+    std::cout << "Rop Step Size: ";
+    std::cin >> input;
+    gpStatePublisher->SetRopStepSize(input);
+}
+
+void top_level_menu()
+{
+    char choice;
+
+    do
+    {
+        std::cout << std::endl;
+        std::cout << "Objective State Publisher" << std::endl;
+        std::cout << "------------------------" << std::endl;
+        std::cout << "1. Publish drilling parameters" << std::endl;
+        std::cout << "q. exit" << std::endl;
+        std::cout << "option: ";
+        std::cin >> choice;
+        std::cout << std::endl;
+
+        switch (choice)
+        {
+            case 'q':
+                gTerminate = true;
+
+                if (threadId.joinable())
+                {
+                    threadId.join();
+                }
+                break;
+            case '1':
+                set_steady_state_window();
+                set_steady_state_minimum();
+                set_rpm_step_size();
+                set_rop_step_size();
+                gpStatePublisher->PublishSample();
+                break;
+        }
+    } while (gTerminate == false);
+
+}
+
+int32_t main(int32_t argc, char **argv)
+{
+    cli::Parser              parser(argc, argv);
+    int32_t                  domain;
+
+	register_signal_handler();
+
+    parser.set_optional<int32_t>("d", "domain", 100, "DDS Domain.");
+    parser.run_and_exit_if_error();
+
+    domain = parser.get<int32_t>("d");
+
+    CDomainParticipant::Instance()->SetQosFile("USER_QOS_PROFILES.xml", "EdgeBaseLibrary", "EdgeBaseProfile");
+    CDomainParticipant::Instance()->Create(domain);
+
+    gpStatePublisher = new CRoptimizerConfigurationRequestPublisher::CConfigurationStateRequestPublisher();
+
+    if (gpStatePublisher->Create(domain) == true)
+    {
+        gpStatePublisher->CreateInstance();
+        top_level_menu();
+    }
+
+    std::cout << "Waiting for thread: "  << std::endl;
+
+    if (threadId.joinable())
+    {
+        threadId.join();
+    }
+
+    gpStatePublisher->Destroy();
+}
